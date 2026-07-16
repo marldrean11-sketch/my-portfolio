fd409db9775ecca0675b32d059e5fe17eb56d82f
marldrean11-sketch <marldrean11@gmail.com>
2026-07-16 21:11:28 +0800

    Add back to top button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Welcome to my Portfolio!")
+const topBtn = document.getElementById("topBtn");
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
